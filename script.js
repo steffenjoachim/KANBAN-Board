e30e0fede2e662040a443a867ee3185d10bfe917
Bünyamin Ilhan <ilhan.buenyamin@gmail.com>
2023-05-31 12:27:20 +0200

    add header and footer

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function togglePasswordVisibility() {
     let passwordIcon = document.getElementById('lock-icon-container');
     let input = document.getElementById('input').value;
     if (input.length >= 1) {
-        passwordIcon.innerHTML = '<img onclick="visibilPassword()" class="password-icon" src="img/show.svg" alt="">';
+        passwordIcon.innerHTML = '<img onclick="visibilPassword()" class="password-icon" src="/asssets/img/show.svg" alt="">';
     } else {
-        passwordIcon.innerHTML = '<img id="lock-icon" src="./img/lock-icon.svg" alt="lock">';
+        passwordIcon.innerHTML = '<img id="lock-icon" src="asssets/img/lock-icon.svg" alt="lock">';
     }
 }
 document.getElementById('input').addEventListener('input', togglePasswordVisibility);
@@ -21,7 +21,7 @@ function visibilPassword() {
     let passwordIcon = document.getElementById('lock-icon-container');
 
     visiblePasswordInput.type = 'text'; // Ändere den Eingabetyp auf "text", um das Passwort sichtbar zu machen
-    passwordIcon.innerHTML = '<img onclick="hidePassword()" class="password-icon" src="img/hide.svg" alt="">';
+    passwordIcon.innerHTML = '<img onclick="hidePassword()" class="password-icon" src="/asssets/img/hide.svg" alt="">';
 }
 
 function hidePassword() {
@@ -32,14 +32,14 @@ function checkmarkChecked() {
     let checkMarkContainer = document.getElementById('checkmark-container');
     checkMarkContainer.innerHTML = '';
     checkMarkContainer.innerHTML += `
-    <img onclick="checkmarkUnChecked()" src="img/checked_full.svg" alt="">
+    <img onclick="checkmarkUnChecked()" src="/asssets/img/checked_full.svg" alt="">
 `
 }
 
 function checkmarkUnChecked() {
     let checkMarkContainer = document.getElementById('checkmark-container');
     checkMarkContainer.innerHTML = '';
-    checkMarkContainer.innerHTML = `<img class="checkbox-empty" onclick="checkmarkChecked()" src="img/checkmark_container.svg" alt="">`
+    checkMarkContainer.innerHTML = `<img class="checkbox-empty" onclick="checkmarkChecked()" src="/asssets/img/checkmark_container.svg" alt="">`
 }
 
 function signIn() {
@@ -51,24 +51,28 @@ function signIn() {
 
 function generateSignInHtml() {
     return `
-    <div class="arrow-container"><a href="index.html"><img onclick="" src="img/back_arrow.svg"></a></div>
+    <div class="arrow-container">
+        <a href="index.html">
+            <img src="/asssets/img/back_arrow.svg">
+        </a>
+    </div>
     <h1 class="sign-in-headline">Sign in</h1>
-    <img src="./img/line.svg" alt="Trennlinie">
+    <img src="/asssets/img/line.svg" alt="Trennlinie">
     <form action="onsubmit">
         <div class="input-field">
             <input class="e-mail" itemid="email" type="email" name="" required placeholder="Email">
-            <img src="./img/email-icon.svg" alt="e-mail icon">
+            <img src="/asssets/img/email-icon.svg" alt="e-mail icon">
         </div>
         <div class="input-field">
             <input id="input" class="password" type="password" required placeholder="Password">
             <div id="lock-icon-container">
-                <img id="lock-icon" src="./img/lock-icon.svg" alt="lock">
+                <img id="/lock-icon" src="asssets/img/lock-icon.svg" alt="lock">
             </div>
         </div>
         <div class="input-field">
             <input id="input" class="password" type="password" required placeholder="Confirm Password">
             <div id="lock-icon-container">
-                <img id="lock-icon" src="./img/lock-icon.svg" alt="lock">
+                <img id="lock-icon" src="/asssets/img/lock-icon.svg" alt="lock">
             </div>
         </div>
         <div class="btn-container">
@@ -78,5 +82,6 @@ function generateSignInHtml() {
 
         </div>
     </form>
+
 `
 }
