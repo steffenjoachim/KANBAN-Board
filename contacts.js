d643bda8984a5ed002883f3e0cad13d20507ea07
steffenjoachim <steffenjoachim.schanze@gmail.com>
2023-06-07 09:16:09 +0200

    mobil display functions partial

--- contacts.js
+++ contacts.js
@@ -4,8 +4,9 @@
  */
 
 function openEditContacts(){
-    document.getElementById('edit-contact').classList.remove('d-none');
+    document.getElementById('edit-contact').classList.add('slide-in');
 }
+
 
 /**
  *  This function is used to close the 'Edit Contact' page. The contact displayed before will be shown again.
@@ -13,7 +14,7 @@ function openEditContacts(){
  */
 
 function closeEditContacts(){
-    document.getElementById('edit-contact').classList.add('d-none');
+    document.getElementById('edit-contact').classList.remove('slide-in');
 }
 
 /**
@@ -35,12 +36,45 @@ function changePenColorToBlack(){
 }
 
 /**
- * This function is used to open the contact details of the clicked on contact on the right side in the desktop version.
+ * This function is used to open the contact details of the clicked on contact, as complete overlay in the mobile version and on the right side in the tablet and desktop version. Since the click on event is started by clicking on the same element in all versions the if-request is used to determin in which way the contact details will be displayed.
  * 
  */
 
 function openContactDetails(){
-    const desktopHidden = document.getElementById('desktop-hidden');
-    desktopHidden.classList.remove('d-none');
-    desktopHidden.classList.add('slide-in');
+    let windowWidth = window.innerWidth;
+    if (windowWidth <= 768) {
+        document.getElementById('content-big-contact').classList.remove('d-none');
+        document.getElementById('content-big-contact').classList.add('slide-in2');
+    } else {
+        document.getElementById('desktop-hidden').classList.add('slide-in');
+    }   
+}
+
+/**
+ * This function is used to close the contact details of the clicked on contact in the mobile version only.
+ * 
+ */
+
+function closeContactDetails(){
+    document.getElementById('content-big-contact').classList.add('d-none');
+    document.getElementById('content-big-contact').classList.remove('slide-in2');
+
+}
+
+/**
+ * This function is used to open the add contact overlay in the desktop version.
+ * 
+ */
+
+function openAddContactsOverlay(){
+    document.getElementById('add-contact').classList.add('slide-in');
+}
+
+/**
+ * This function is used to close the add contact overlay in the desktop version.
+ * 
+ */
+
+function closeAddContactsOverlay(){
+    document.getElementById('add-contact').classList.remove('slide-in');
 }
