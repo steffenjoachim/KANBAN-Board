e74966603fe2dc4bd49568f41c47215b233a4163
Your Name git config --global user.email <mohamedtam914@gmail.com>
2023-06-15 14:09:57 +0200

    update new Board

--- js/summary.js
+++ js/summary.js
@@ -94,3 +94,8 @@ function displayTodoCount(todos) {
   }
   
 
+/////   Funktion die bei einem Klick auf ein div-Element zur "board.html"-Seite weiterleitet /////
+
+  function redirectToBoard() {
+    window.location.href = 'board.html';
+  }
